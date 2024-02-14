686c004b40761ca8b1ce2e58810c1dbfd91f75f0
harshubari <121845828+harshubari@users.noreply.github.com>
2024-02-14 09:52:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+// script.js
+
+// Function to capitalize the content of the input field
+function capitalizeName() {
+    var inputField = document.getElementById("fname");
+    inputField.value = inputField.value.toUpperCase();
+}
+document.getElementById("fname").addEventListener("blur", capitalizeName);
